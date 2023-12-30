90dfd445f3943499e3e082c4317c6bb79072976b
Noah <nordqvist.gaming@gmail.com>
2023-12-30 17:25:30 +0100

    Add player model and block out first level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
+@export var speed : float = 20.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_accept"):
+			position.y += delta * speed
+	
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
