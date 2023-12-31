1aa3c58aab9e9110dc3c456c1a92683dd838acf9
Noah <nordqvist.gaming@gmail.com>
2023-12-31 12:30:43 +0100

    Add input map

--- player/player.gd
+++ player/player.gd
@@ -4,11 +4,11 @@ extends RigidBody3D
 @export var agility : float = 100.0
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * thrust)	
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, agility * delta))	
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -agility * delta))
